15389366cb3e66daaa50c8399ddb78fc777636c8
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-23 21:46:01 -0500

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, except: [:edit, :update, :destroy] do
+   # /lists/
+    collection do
+      # 'lists/top' otra ruta
+      # get :top
+    end
+    member do
+      # 'lists/1/chef'
+      # get :chef
+    end
+    # lists/1/bookmarks/new
+    resources :bookmarks, only: [:new, :create]
+  end
+  # /bookmarks/:id
+  resources :bookmarks, only: [:destroy]
 end
